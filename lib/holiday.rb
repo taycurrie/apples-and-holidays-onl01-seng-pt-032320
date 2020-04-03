f0f03b988e5e60b98ee845be8e28aa6e834437d0
sunny-tier-6236 <taycurrie@gmail.com>
2020-04-03 18:02:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,6 @@
 require 'pry'
-
-def 
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
